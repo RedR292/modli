ac41b3358891acc1a27b793dd542d0018ba87f86
John Richardson <johnrichardson5152001@gmail.com>
2025-12-23 15:43:40 -0500

    major revision

--- src/ModManagers/Fallout 4.mm
+++ src/ModManagers/Fallout 4.mm
@@ -3,20 +3,21 @@ checkTrivialMod() {
 	##Check if MOD is an ENB mod
 	7z l "$MOD" | grep -iq enb
 	if [ $? -eq 0 ]; then
-		echo "modli: WARNING: This mod may be an ENB mod and cannot be installed as a raw mod. It must be installed with a custom script"
+		echo "modli: WARNING: This mod may be an ENB mod and cannot be installed as a raw mod. It must be installed with a custom script" >&2
 		return 1
 	fi
 	##Check if MOD is a script extender
 	7z l "$MOD" | grep -q 'f4se_loader.exe'
 	if [ $? -eq 0 ]; then
-		echo "modli: WARNING: this is a script extender and cannot be installed as a raw mod It must be installed with a custom script"
+		echo "modli: WARNING: this is a script extender and cannot be installed as a raw mod. It must be installed with a custom script" >&2
 		return 1
 	fi
-	
+
 }
 
 validatemod() {
-	##Matches 'yyyy-mm-dd hh:mm:ss D' at the start of each line. Example match: 2000-01-01 00:00:00 D 
+	##Matches 'yyyy-mm-dd hh:mm:ss (D or '.')' at the start of each line. Example match: 2000-01-01 00:00:00 .
+	##Used to isolate files from the directories
 	regex="^[[:digit:]]\{4\}\(-[[:digit:]]\{2\}\)\{2\} [[:digit:]]\{2\}:[[:digit:]]\{2\}:[[:digit:]]\{2\} [D.]"
 	contents="$(7z l "$MOD" | tac | grep -e "$regex" | awk '{print $NF}')"
 	##Ensures MOD is a 7z file
@@ -31,20 +32,20 @@ validatemod() {
 	if [[ ! "$(echo "$contents" | sed -n 1p)" == "Data" ]]; then
 		return 3
 	fi
-	
+
         #mod is valid
 	return 0
 }
 
 #process the options
-case $cmd in 
+case $cmd in
 	-i)
 		install
 		if [[ $? -ne 0 ]]; then
-			echo "modli: ERR: $mod was not installed." >&2
+			echo "modli: ERR: $MOD was not installed." >&2
 			return 1
 		else
-			echo "modli: $mod was installed to $GAME_NAME"
+			echo "modli: $(basename "$MOD") was installed to $GAME_NAME"
 		fi
 		shift 2
 		;;
@@ -80,13 +81,17 @@ case $cmd in
 			echo "modli: ERR: $mod was not removed from $GAME_NAME's stage"
 			return 1
 		else
-			echo "modli: $mod was removed from mods.stage"
+			echo "modli: $mod was removed from $GAME_NAME's stage"
 		fi
 		return 0
 		shift 2
 		;;
 	--Launch)
 		Launch
+		shift 2
+		;;
+	--Remove)
+		Remove
 		shift 2
 		;;
 	*)
